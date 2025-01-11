cbdd7f5293593c9b49b39fa27504975a1179710c
shadil-rayyan <shadilrayyan2@gmail.com>
2025-01-11 17:11:27 +0000

    made change to nextconfig gile missed /

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output:'export',
-  basepath:'littleEatery',
-  assetPrefix:'littleEatery'
+  basepath:'/littleEatery',
+  assetPrefix:'/littleEatery'
 };
 
 module.exports = nextConfig;
